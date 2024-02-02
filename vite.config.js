ad3d3fb435d40f874302e0b16dcbf7f889c0f559
Lenny <lenny.l.miller@gmail.com>
2024-02-02 12:45:51 -0800

    the one true god

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,16 @@ import { defineConfig, loadEnv } from 'vite';
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   const config = {
-    esbuild: {
-      minify: true,
-    },
-    build: {
-      lib: {
-        entry: './src/index.js',
-        name: 'web-components',
-        fileName: (format) => `web-components.${format}.js`,
-      },
-    },
+    // esbuild: {
+    //   minify: true,
+    // },
+    // build: {
+    //   lib: {
+    //     entry: './src/index.js',
+    //     name: 'web-components',
+    //     fileName: (format) => `web-components.${format}.js`,
+    //   },
+    // },
     plugins: [],
     server: {
       proxy: {
@@ -25,17 +25,17 @@ export default ({ mode }) => {
           secure: false,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
+        // configure: (proxy, _options) => {
+        //   proxy.on('error', (err, _req, _res) => {
+        //     console.log('proxy error', err);
+        //   });
+        //   proxy.on('proxyReq', (proxyReq, req, _res) => {
+        //     console.log('Sending Request to the Target:', req.method, req.url);
+        //   });
+        //   proxy.on('proxyRes', (proxyRes, req, _res) => {
+        //     console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+        //   });
+        // },
       },
     },
   };
